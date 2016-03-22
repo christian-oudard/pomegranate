88715c9e4c868edef5438a5155009f07e782a9fe
Christian Oudard <christian.oudard@gmail.com>
2016-03-22 14:56:23 -0700

    Add a command line argument for a custom number of minutes.

--- pomegranate.hs
+++ pomegranate.hs
@@ -1,3 +1,4 @@
+import System.Environment (getArgs)
 import Control.Concurrent (threadDelay)
 import Data.Time.Clock (getCurrentTime, diffUTCTime)
 import System.Random.MWC (createSystemRandom)
@@ -5,23 +6,39 @@ import System.Random.MWC.Distributions (normal)
 
 import Cancel (onCancel)
 
-main = do
-  g <- createSystemRandom
-  x <- normal (35*60) (4*60) g
+deviationRatio = 1/8
+defaultWaitMinutes = 30
 
+main = do
+  -- Determine how long to wait for.
+  timerMinutes <- parseDelay
+  putStrLn $ "Timer set for about " ++ show (round timerMinutes) ++ "m."
+
+  g <- createSystemRandom
+  x <- normal (timerMinutes*60) (timerMinutes*60*deviationRatio) g
+
+  -- Wait for the specified amount of time, handling early cancellation gracefully.
   start <- getCurrentTime
 
   onCancel $ do
     putStrLn ""
     end <- getCurrentTime
     let diff = diffUTCTime end start
-    putStrLn $ "Cancelled after " ++ showMinSec diff ++ "."
+     in putStrLn $ "Cancelled after " ++ showMinSec diff ++ "."
 
   delaySeconds x
-
   end <- getCurrentTime
   let diff = diffUTCTime end start
-  putStrLn $ "Done after " ++ showMinSec diff ++ "."
+   in putStrLn $ "Done after " ++ showMinSec diff ++ "."
+
+parseDelay :: IO (Double)
+parseDelay = do
+  args <- getArgs
+  if (length args == 0)
+    then do
+      return defaultWaitMinutes
+    else do
+      return $ read (args !! 0)
 
 delaySeconds :: RealFrac a => a -> IO()
 delaySeconds s = threadDelay $ round $ s*second
@@ -35,8 +52,9 @@ showMinSec seconds =
 
 div' :: (Real a, Integral b) => a -> a -> b
 div' n d = floor ((toRational n) / (toRational d))
+
 mod' :: (Real a) => a -> a -> a
 mod' n d = n - (fromInteger f) * d where f = div' n d
+
 divMod' :: (Real a, Integral b) => a -> a -> (b,a)
 divMod' n d = (div' n d, mod' n d)
-
